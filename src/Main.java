3bb18d8af97c0ee9b97025945ff1844e8c62de82
Sergei Prutsakov <psn206@mail.ru>
2022-05-30 21:09:53 +0300

    correction on comments

--- src/Main.java
+++ src/Main.java
@@ -6,28 +6,24 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
 
-        int numberThreads = 4;
-        List<Callable<Integer>> listCallable = new ArrayList();
-        Callable<Integer> myCallable1 = new MyCallable(300);
-        Callable<Integer> myCallable2 = new MyCallable(200);
-        Callable<Integer> myCallable3 = new MyCallable(100);
-        Callable<Integer> myCallable4 = new MyCallable(400);
-        listCallable.add(myCallable1);
-        listCallable.add(myCallable2);
-        listCallable.add(myCallable3);
-        listCallable.add(myCallable4);
-         ExecutorService threadPool = Executors.newFixedThreadPool(numberThreads);
+        final int numberThreads = 4;
+        List<MyCallable> listCallable = new ArrayList();
+        for (int i = 0; i < numberThreads; i++) {
+            listCallable.add(new MyCallable(i + 2));
+        }
+        ExecutorService threadPool = Executors.newFixedThreadPool(numberThreads);
+        System.out.println("Включаем все потоки:");
         List<Future<Integer>> taskResults = threadPool.invokeAll(listCallable);
-            System.out.println("Потоки с количеством выводимых сообщений:");
-        for (Future<Integer> taskResult : taskResults
-        ) {
-            System.out.println("Количество выводимых сообщений: " + taskResult.get());
+        for (Future<Integer> taskResult : taskResults) {
+            System.out.println("Количество выводимых сообщений потоком " + "= " + taskResult.get());
         }
         int taskResult = threadPool.invokeAny(listCallable);
+        try {
+            Thread.sleep(2000); // усыпляем на 2 секунды поток main
+        } catch (InterruptedException ignored) {
+        }
+        System.out.println("Самый быстрый поток  выводит " + taskResult + " сообщения ");
         threadPool.shutdown();
-        System.out.println("Самый быстрый поток с " + taskResult + " выводимых сообщений");
-        threadPool.shutdown();
-
     }
 }
 
--- src/MyCallable.java
+++ src/MyCallable.java
@@ -9,14 +9,10 @@ public class MyCallable implements Callable<Integer> {
 
     @Override
     public Integer call() throws Exception {
-
-        try {
-                Thread.sleep(this.spentTime);
-
-
-        } catch (InterruptedException err) {
-
-          }
+        for (int i = 0; i < spentTime+1; i++) {
+            System.out.println("Я поток " + Thread.currentThread().getName() +
+                    ". Печатаю сообщение  " + i + " из " + spentTime);
+        }
         return  this.spentTime;
     }
 }
